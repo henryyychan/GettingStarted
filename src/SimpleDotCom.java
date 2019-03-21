999258b929d27d20b2c9818cf1b7c32589b42435
henryyychan <henryyychan@163.com>
2019-03-21 15:35:30 +0800

    first version of SimpleDotCom.

--- src/SimpleDotCom.java
+++ src/SimpleDotCom.java
@@ -1,7 +1,26 @@
 public class SimpleDotCom {
+    private int[] locationCells;
+    private int numOfHits = 0;
 
-    public String checkYourself(String stringGuess) {
-        // check yourself
+    public void setLocationCells(int[] locs) {
+        locationCells = locs;
     }
 
-}
+    public String checkYourself(String stringGuess) {
+        int guess = Integer.parseInt(stringGuess);
+        String result = "miss";
+        for (int cell : locationCells) {
+            if (guess == cell) {
+                result = "hit";
+                numOfHits++;
+                break;
+            }
+        }// out of the loop
+
+        if (numOfHits == locationCells.length) {
+            result = "kill";
+        }
+        System.out.println(result);
+        return result;
+    }// close method
+}// close class
